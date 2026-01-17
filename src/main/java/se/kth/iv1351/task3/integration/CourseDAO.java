5993a65316e8627ab8f2683949e44b9bf7e5a052
ebbaelvelid <ebba@elvelid.com>
2026-01-17 15:51:58 +0100

    Refactor CourseDAO methods to use allocated hours and follow naming pattern

--- src/main/java/se/kth/iv1351/task3/integration/CourseDAO.java
+++ src/main/java/se/kth/iv1351/task3/integration/CourseDAO.java
@@ -1,9 +1,14 @@
 package integration;
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.CourseHeaderDTO;
 
 public class CourseDAO {
 
-    public ResultSet findCourseHeader(Connection conn, String instanceId) throws SQLException {
+    public CourseHeaderDTO readCourseHeader(Connection conn, String instanceId) throws SQLException {
         String sql = """
         SELECT
             cl.course_code,
@@ -15,48 +20,48 @@ public class CourseDAO {
         WHERE ci.instance_id = ?
         """;
 
-        PreparedStatement ps = conn.prepareStatement(sql);
-        ps.setString(1, instanceId);
-        return ps.executeQuery();
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, instanceId);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new CourseHeaderDTO(
+                        rs.getString("course_code"),
+                        rs.getString("instance_id"),
+                        rs.getString("study_period")
+                    );
+                }
+                return null;
+            }
+        }
     }
 
-    public double findActualAllocatedHours(Connection conn, String instanceId) throws SQLException {
+    public double readActualAllocatedHours(Connection conn, String instanceId) throws SQLException {
         String sql = """
         SELECT COALESCE(SUM(
             CASE
                 WHEN ta.activity_name IN ('Lecture','Tutorial','Lab','Seminar','Others')
-                    THEN pa.planned_hours * ta.factor
-                WHEN ta.activity_name = 'Administration'
-                    THEN pa.planned_hours + ta.factor * ci.num_students + 2 * cl.hp
-                WHEN ta.activity_name = 'Examination'
-                    THEN pa.planned_hours + ta.factor * ci.num_students
+                    THEN a.allocated_hours * ta.factor
+                WHEN ta.activity_name IN ('Administration','Examination')
+                    THEN a.allocated_hours
                 ELSE 0
             END
         ), 0) AS total
         FROM allocations a
-        JOIN planned_activity pa
-            ON a.id_teaching = pa.id_teaching
-           AND a.instance_id = pa.instance_id
-        JOIN teaching_activity ta
-            ON pa.id_teaching = ta.id
-        JOIN course_instance ci
-            ON a.instance_id = ci.instance_id
-        JOIN course_layout cl
-            ON ci.id_layout = cl.id
+        JOIN teaching_activity ta ON a.id_teaching = ta.id
         WHERE a.instance_id = ?
         """;
 
         try (PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, instanceId);
-            ResultSet rs = ps.executeQuery();
-            rs.next();
-            return rs.getDouble("total");
+            try (ResultSet rs = ps.executeQuery()) {
+                rs.next();
+                return rs.getDouble("total");
+            }
         }
     }
 
-    public double findPlannedHours(Connection conn, String instanceId) throws SQLException {
-        String sql
-                = """
+    public double readPlannedHours(Connection conn, String instanceId) throws SQLException {
+        String sql = """
             SELECT SUM(
                 CASE
                     WHEN ta.activity_name IN
@@ -81,37 +86,38 @@ public class CourseDAO {
 
         try (PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, instanceId);
-            ResultSet rs = ps.executeQuery();
-            rs.next();
-            return rs.getDouble("total");
+            try (ResultSet rs = ps.executeQuery()) {
+                rs.next();
+                return rs.getDouble("total");
+            }
         }
     }
 
-    public int findStudentsForUpdate(Connection conn, String instanceId) throws SQLException {
-        String sql
-                = "SELECT num_students "
-                + "FROM course_instance "
-                + "WHERE instance_id = ? "
-                + "FOR UPDATE";
+    public int readStudentsForUpdate(Connection conn, String instanceId) throws SQLException {
+        String sql = """
+                SELECT num_students
+                FROM course_instance
+                WHERE instance_id = ?
+                FOR UPDATE
+                """;
 
         try (PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, instanceId);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                return rs.getInt("num_students");
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt("num_students");
+                }
+                throw new SQLException("Course instance not found");
             }
-            throw new SQLException("Course instance not found");
         }
     }
 
-    public void updateStudents(Connection conn,
-            String instanceId,
-            int newValue) throws SQLException {
-
-        String sql
-                = "UPDATE course_instance "
-                + "SET num_students = ? "
-                + "WHERE instance_id = ?";
+    public void updateStudents(Connection conn, String instanceId, int newValue) throws SQLException {
+        String sql = """
+                UPDATE course_instance
+                SET num_students = ?
+                WHERE instance_id = ?
+                """;
 
         try (PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setInt(1, newValue);
